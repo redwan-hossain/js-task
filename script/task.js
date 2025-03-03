a4eaca8b3d0a66dc8c482921dd3a6cd8290eb016
redwan-hossain <rfredwan0@gmail.com>
2025-03-03 23:05:33 +0600

    color change button added

--- script/task.js
+++ script/task.js
@@ -22,3 +22,9 @@ for(btn of cardBtn){
     })
     
 }
+
+
+function getRandomHexColor() {
+    const colorBtn =  '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    document.body.style.backgroundColor = colorBtn;
+}
