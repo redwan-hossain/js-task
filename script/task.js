7430d817a134dd25de84a309934e97e807b6bec8
redwan-hossain <rfredwan0@gmail.com>
2025-03-03 22:58:12 +0600

    js adding

--- script/task.js
+++ script/task.js
@@ -1,8 +1,23 @@
 const cardBtn = document.getElementsByClassName('cardBtn');
+let assignNumber =  parseInt(document.getElementById('assignNumber').innerText);
+const dateDay = new Date();
+const dateFormate = dateDay.toLocaleString("en-GB", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+});
+const weekDay = dateDay.toLocaleDateString(undefined, { weekday: 'short' });
+document.querySelector("#daySection").innerHTML = weekDay;
+document.querySelector("#dateSpan").innerHTML = dateFormate;
 
 for(btn of cardBtn){
     btn.addEventListener('click', function(event) {
-        console.log('hello');
+        alert('Board Updated Successfully')
+        assignNumber--;
+        document.getElementById('assignNumber').innerText =  "0"+ assignNumber;
+        document.getElementById('headerNumber').innerText++;
+        
+        event.target.setAttribute('disabled',true)
         
     })
     
