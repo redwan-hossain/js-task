e44d6be63368b34b86d3c94e71a97723ba1dab2b
redwan-hossain <rfredwan0@gmail.com>
2025-03-03 22:03:19 +0600

    first commit

---
+++ script/task.js
@@ -0,0 +1,9 @@
+const cardBtn = document.getElementsByClassName('cardBtn');
+
+for(btn of cardBtn){
+    btn.addEventListener('click', function(event) {
+        console.log('hello');
+        
+    })
+    
+}
